ab211e9151153637d1f78382553d0c7245c948cb
elf <shadenitey@Gmail.com>
2021-01-27 15:36:58 +0000

    fix identation errors, and add setup functions

--- cogs/profile.py
+++ cogs/profile.py
@@ -12,18 +12,18 @@ class Profile(commands.Cog):
     self.db = sqlite.Database()
     self.username_regex = r"^\w+"
 
- def check_existing_profile(self, user_id):
+  def check_existing_profile(self, user_id):
    data = self.db.fetchrow("SELECT * FROM profile_name WHERE user_id=?", (user_id,))
    if data:
        return data["profile_name"]
-    else:
+   else:
        return None
 
 
- @commands.command(
+  @commands.command(
    name="start"
- )
- async def start_(self, ctx):
+  )
+  async def start_(self, ctx):
    """ Create a profile! """
    profile_exists = self.check_existing_profile(ctx.author.id)
    if profile_exists:
@@ -32,28 +32,31 @@ class Profile(commands.Cog):
             )
 
    start_msg = await ctx.send(f"Hello {ctx.author.mention}! Please choose a username for your profile!")
-        confirm = random.randint(10000, 99999)
+   confirm = random.randint(10000, 99999)
 
 
-        def check_confirm(m):
-            if (m.author == ctx.author and m.channel == ctx.channel):
-                if (m.content.startswith(str(confirm))):
-                    return True
-            return False
+   def check_confirm(m):
+     if (m.author == ctx.author and m.channel == ctx.channel):
+       if (m.content.startswith(str(confirm))):
+           return True
+       return False
 
-    setname = user.content.split(" ")[0]
+   setname = user.content.split(" ")[0]
+   confirm_msg = await ctx.send(
+   f"Alright **{ctx.author.name}**, do you confirm that your username is {setname}?\nType `{confirmcode}` to confirm this choice\n")
 
-
-    confirm_msg = await ctx.send(
-            f"Alright **{ctx.author.name}**, do you confirm that your username is {setname}?\nType `{confirmcode}` to confirm this choice\n"
-        )
-
-        try:
-            user = await self.bot.wait_for('message', timeout=30.0, check=check_confirm)
-        except asyncio.TimeoutError:
-            return await confirm_msg.edit(
-                content=f"~~{confirm_msg.clean_content}~~\n\nStopped process..."
-            )
+   try:
+     user = await self.bot.wait_for('message', timeout=30.0, check=check_confirm)
+   except asyncio.TimeoutError:
+        return await confirm_msg.edit(
+            content=f"~~{confirm_msg.clean_content}~~\n\nStopped process...")
 
         self.db.execute("INSERT INTO profile_name VALUES (?, ?)", (ctx.author.id, setname))
         await ctx.send(f":checkmark: Your username is now set in my database!")
+
+
+
+
+
+def setup(bot):
+  bot.add_cog(Profile(bot))
