05328779d98373f3c70bb2885903c80bb38e7d30
elf <49889930+q7n@users.noreply.github.com>
2021-01-31 16:40:20 -0500

    add batch setup

---
+++ setup.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Starting setup...
+
+pip install -r requirements.txt
+
+echo Imported required packages.
+
+python main.py
+
+echo Completed process, close this window and check if your bot is online!
+
+PAUSE
