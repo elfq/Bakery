2ec9d367548485f48e91cb4887bd601f967647f4
elf <49889930+q7n@users.noreply.github.com>
2021-01-31 16:41:18 -0500

    organize the code

--- cogs/bakery.py
+++ cogs/bakery.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
 from utils.database import create_tables, sqlite
+from utils import default, checks
 import random
 import asyncio
 import re
-
 
 tables = create_tables.creation(debug=True)
 if not tables:
@@ -16,18 +16,30 @@ class Bakery(commands.Cog):
     self.db = sqlite.Database()
     self.username_regex = r"^[^@]*$"
 
-  def check_existing_profile(self, user_id):
-   data = self.db.fetchrow("SELECT * FROM Accounts WHERE user_id=?", (user_id,))
-   if data:
-     return data["bakery_name"]
-   return None
+  def bakery_name_exists(self, user_id):
+        data = self.db.fetchrow("SELECT * FROM Accounts WHERE user_id=?", (user_id,))
+        if data:
+            return data["bakery_name"]
+        else:
+            return None
+
+  @commands.group(invoke_without_command=True)
+  async def bakery(self, ctx):
+    embed = discord.Embed(
+      title = "Bakery - Menu",
+      description = "Commands that relate to **Bakery**, to use a command, type `b!bakery <section>`.",
+      color = discord.Colour.blurple()
+    )
+
+    embed.set_thumbnail(url=self.bot.user.avatar_url)
+    embed.add_field(name="Sections", value="**start**\n**view**")
+    await ctx.reply(embed=embed)
 
 
-  @commands.command(
-   name="start")
+  @bakery.command(name="start")
   async def start_(self, ctx):
    """ Create a bakery! """
-   bakery_exists = self.check_existing_profile(ctx.author.id)
+   bakery_exists = self.bakery_name_exists(ctx.author.id)
    if bakery_exists:
      return await ctx.send(f":x: You've already created a bakery!")
 
@@ -53,9 +65,6 @@ class Bakery(commands.Cog):
          return await start_content.edit(
             content=f"~~{start_content.clean_content}~~\n\n:x: Bakery creation failed!")
 
-   
-
-
    setname = user.content
 
    confirm_msg = await ctx.send(f"Okay {ctx.author.mention}, are you sure you want to set your Bakery's Name to **`{setname}`**? Please type `{confirm}` if you're sure!")
@@ -67,21 +76,41 @@ class Bakery(commands.Cog):
             content=f"~~{confirm_msg.clean_content}~~\n\n:x: Bakery creation process stopped...")
 
    
-   
+   channel = self.bot.get_channel(805210381466599424)
    self.db.execute("INSERT INTO Accounts VALUES (?, ?)", (ctx.author.id, setname))
    await ctx.send(f"✅ Your Bakery has been created!")
+   embed = discord.Embed(
+     title = "Bakery Creation!",
+     description = f"**Bakery Name:** {setname}\n**Creator:** {ctx.author.id}\n**Creator's Name:** {ctx.author}",
+     color = discord.Colour.blurple()
+   )
+   embed.set_thumbnail(url=ctx.author.avatar_url)
+   await channel.send(embed=embed)
 
 
-  @commands.command(name="bakery")
-  async def profile_(self, ctx):
-    db_bakeryname = self.check_existing_profile(ctx.author.id)
+  @bakery.command(name="view")
+  async def view_(self, ctx):
+    """ View a bakery! """
+    bakery_exists = self.bakery_name_exists(ctx.author.id)
     embed = discord.Embed(
       title=f"{ctx.author}'s' Bakery",
-      description=f"**Name:** {db_bakeryname}\n**Bake Bucks:** 0 \n**Level:** 0/100\n**Items Baked:** 0",
+      description=f"**Name:** {bakery_exists}\n**Bake Bucks:** 0 \n**Level:** 0/100\n**Items Baked:** 0",
       color = discord.Colour.blurple())
 
     embed.set_footer(text="BakeryBot - v0.1")
     await ctx.reply(embed=embed)
+
+  
+  @commands.command()
+  @commands.check(checks.is_moderator)
+  async def delete(self, ctx, user: discord.Member,reason='None given.'):
+   self.db.execute("DELETE FROM Accounts WHERE user_id=?", (user.id,))
+   await ctx.send(f"✅ Successfully deleted {user}'s bakery for `{reason}`'")
+   await user.send(f"Hello, your bakery has been deleted for violating the bot's rules, if you feel like this is false, then join https://discord.gg/sDaP9NU and send our moderators a message!\n\nAction by: {ctx.author.mention}\nDeletion Message: `{reason}`")
+
+
+   
+
 
 
 
