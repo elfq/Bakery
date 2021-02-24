b9d7462098bbc5880f7edf7534f6a887b248dc57
elf <shadenitey@Gmail.com>
2021-02-24 06:54:41 +0000

    delete shard.py and default.py since templatebot is imported, remove unnesscery stuff from checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -2,28 +2,10 @@ import discord
 import os
 from os import environ
 from discord.ext import commands
-from utils import default
 
 
-owners = environ.get("DEVELOPERS")
+owners = 738604939957239930
 
 
 def is_owner(ctx):
-    """ Checks if the author is one of the owners """
     return ctx.author.id in owners
-
-
-async def check_permissions(ctx, perms, *, check=all):
-    """ Checks if author has permissions to a permission """
-    if ctx.author.id in owners:
-        return True
-
-    resolved = ctx.channel.permissions_for(ctx.author)
-    return check(getattr(resolved, name, None) == value for name, value in perms.items())
-
-
-def has_permissions(*, check=all, **perms):
-    """ discord.Commands method to check if author has permissions """
-    async def pred(ctx):
-        return await check_permissions(ctx, perms, check=check)
-    return commands.check(pred)
--- utils/shard.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord.ext.commands import AutoShardedBot
-
-
-class Bot(AutoShardedBot):
-    def __init__(self, *args, prefix=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.prefix = prefix
